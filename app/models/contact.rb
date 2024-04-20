4dacc1fdd8c2bc482501e39e893ad8cf95f44200
Audrey_xo <anitaudrey@gmail.com>
2024-04-20 19:29:22 +0100

    Rails app upgraded to rails 7 and Dependencies errors resolution

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 class Contact < MailForm::Base
-  attribute :name,      :validate => true
-  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :message,   :validate => true
-  attribute :nickname,  :captcha  => true
+  attribute :name,      validate: true
+  attribute :email,     validate: /\A([\w.%+-]+)@([\w-]+\.)+(\w{2,})\z/i
+  attribute :message,   validate: true
+  attribute :nickname,  captcha: true
 
   def headers
-  	{
-	  	:subject => "Contact Form",
-	  	:to => "anitaudrey@gmail.com",
-	  	:from => %("#{name}" <#{email}>)
-	}
-	end
+    {
+      subject: 'Contact Form',
+      to: 'anitaudrey@gmail.com',
+      from: %("#{name}" <#{email}>)
+    }
+  end
 end
--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,33 +1,33 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
-
-  describe "GET #home" do
-    it "returns http success" do
+  describe 'GET #home' do
+    it 'returns http success' do
       get :home
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #works" do
-    it "returns http success" do
+  describe 'GET #works' do
+    it 'returns http success' do
       get :works
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #publication" do
-    it "returns http success" do
+  describe 'GET #publication' do
+    it 'returns http success' do
       get :publication
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #contact" do
-    it "returns http success" do
+  describe 'GET #contact' do
+    it 'returns http success' do
       get :contact
       expect(response).to have_http_status(:success)
     end
   end
-
 end
